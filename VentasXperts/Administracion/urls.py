0530ac70ce51a930359f6eb637b13d9322c1e1a8
MelvinMG <melvinmaring.20@gmail.com>
2025-03-25 18:48:40 -0600

    Usuarios y Roles pasados a REST

---
+++ VentasXperts/Administracion/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
+
+# Crear un router para registrar el ViewSet
+router = DefaultRouter()
+router.register(r'users', UserViewSet)  # Registro del UserViewSet
+
+urlpatterns = [
+    path('', include(router.urls)),  # Registrar las URLs del router
+]
