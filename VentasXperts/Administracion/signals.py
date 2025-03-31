b0184cb4eebd25d192c0dc05a5a52c163e6e121e
MelvinMG <melvinmaring.20@gmail.com>
2025-03-30 21:11:42 -0600

    signals acutalizado

--- VentasXperts/Administracion/signals.py
+++ VentasXperts/Administracion/signals.py
@@ -1,10 +1,20 @@
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
 @receiver(post_migrate)
 def create_default_groups(sender, **kwargs):
+    """
+    Crear los grupos por defecto (Administrador, Gerente, Cajero) 
+    y asignar todos los roles a los superusuarios.
+    """
+     
     # Crear los grupos si no existen
-    Group.objects.get_or_create(name='Administrador')
-    Group.objects.get_or_create(name='Gerente')
-    Group.objects.get_or_create(name='Cajero')
+    administrador, created = Group.objects.get_or_create(name='Administrador')
+    gerente, created = Group.objects.get_or_create(name='Gerente')
+    cajero, created = Group.objects.get_or_create(name='Cajero')
+    # Asignar todos los grupos a los superusuarios
+    superusers = User.objects.filter(is_superuser=True)
+
+    for superuser in superusers:
+        superuser.groups.add(administrador, gerente, cajero)
