c05032d8a0f6257337f58125c6d9ca13c0588a55
Tonny <tonny54v@gmail.com>
2025-05-15 14:15:07 -0600

    Se cambio a la ip por defecto para evitar confictos con otros modulos.

--- mobile/app/src/main/java/com/app/ventasxpertsmobile/data/api/ApiClient.kt
+++ mobile/app/src/main/java/com/app/ventasxpertsmobile/data/api/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://192.168.1.20:8000/api/" // se cambio solo para pruebas, omitir en merge
+    private const val BASE_URL = "http://10.0.2.2:8000/api/" // se cambio solo para pruebas, omitir en merge
 
 
     val retrofit: Retrofit = Retrofit.Builder()
